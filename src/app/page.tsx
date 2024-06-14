0194664a3a0508dfa71bc0321342e399ba97d0fa
Saad Nauman <87758531+saadings@users.noreply.github.com>
2024-06-14 18:37:23 +0500

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { GlobeDemo } from "@/components/Globe";
 
 export default function Home() {
   return (
-    <main className="flex select-none flex-col items-center justify-center">
+    <main className="flex h-screen select-none flex-col items-center justify-center bg-black">
       <GlobeDemo />
     </main>
   );
--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -422,9 +422,11 @@ export function GlobeDemo() {
           }}
           className="div"
         >
-          <h2 className="text-center text-xl font-bold text-black dark:text-gray-700 md:text-4xl">
-            Welcome to My World!
-          </h2>
+          <div className="flex justify-center">
+            <h2 className="text-center text-xl font-bold text-black dark:text-gray-200 md:text-4xl">
+              Welcome to the World of Saadings!
+            </h2>
+          </div>
           {/* <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
             This globe is interactive and customizable. Have fun with it :)
           </p> */}
