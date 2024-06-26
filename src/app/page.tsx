49a957f92b6aa85b5c4df506539d0a1ea4c23eae
Saad Nauman <87758531+saadings@users.noreply.github.com>
2024-06-26 11:51:55 +0500

    Initial Commit

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { GlobeDemo } from "@/components/Globe";
-
-export default function Home() {
-  return (
-    <main className="flex h-screen select-none flex-col items-center justify-center bg-black">
-      <GlobeDemo />
-    </main>
-  );
-}
