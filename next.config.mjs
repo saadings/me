3fb499a1ea480f671e4c6501d1e9d4ce5ce33e77
Saad Nauman <87758531+saadings@users.noreply.github.com>
2024-07-09 21:54:27 +0500

    add seo

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,7 @@ export default withSentryConfig(nextConfig, {
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
+  productionBrowserSourceMaps: true,
 
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
